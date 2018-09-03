434b18826d70d39b45bdd0a770d28c902c063716
Daniel Pérez <elwhatha@hotmail.com>
2018-09-03 16:42:23 -0500

    lunes_3

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngAnimate']);
             })
  			.when('/people', {
             	templateUrl : 'pages/people.html',
-            	controller  : 'mainController'
+            	controller  : 'peopleController'
             })
             // route for the about page
             .when('/about', {
@@ -42,9 +42,33 @@ var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngAnimate']);
         // create a message to display in our view
         $scope.message = 'Everyone come and see how good I look!';
     });
-
+    scotchApp.controller('peopleController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    $scope.hoverIn = function(event){
+            //  this.hoverEdit = true;
+            var el = getElement(event);
+            // Do something with element, for example add a class
+            $('#selected').addClass('añadir');
+            console.log('hoverIn ' + el.attr('src'));
+                    $scope.imagen = el.attr('src');
+                    $scope.nombre=el.attr('nombre');
+                    $scope.edad=el.attr('edad');
+                    $scope.carrera=el.attr('carrera');
+                    $scope.universidad=el.attr('universidad');
+                    $scope.hobbies=el.attr('hobbies');
+          };
+    $scope.hoverOut = function(event){
+                    var el = getElement(event);
+            $('#selected').removeClass('añadir');
+        };
+    function getElement(event) {
+        return angular.element(event.srcElement || event.target);
+    }
+    });
     scotchApp.controller('aboutController', function($scope) {
     	$scope.message = 'Look! I am an about page.';
+
 
     /*	$scope.hoverOut = function(event){
     			    var el = getElement(event);
